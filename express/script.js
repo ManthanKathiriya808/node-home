082e1721cb5506d13ffd13ce2c5b0cc31dd269a9
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2025-06-15 12:53:18 +0530

    dsfvsdfg

---
+++ express/script.js
@@ -0,0 +1,50 @@
+const express = require("express")
+
+const app = express()
+
+const port = 8000
+
+app.use((req,res,next)=>{
+    console.log("this is middleware")
+    next()
+})
+app.use((req,res,next)=>{
+    console.log("this is middleware for next time")
+    next()
+})
+
+app.get('/',(req,res)=>{
+    console.log("hellow world")
+    res.send("Hello world")
+})
+
+
+
+app.get('/about',(req,res)=>{
+    // console.log("hellow world")
+    res.send(" this is an About page ")
+})
+app.get('/contact',(req,res)=>{
+    // console.log("hellow world")
+    res.send(" contact page ")
+})
+app.get('/checkout',(req,res,next)=>{
+    return next(new Error("sometthing went wrong"))
+    // res.send(" checkout page ")
+    // console.log("hellow world")
+})
+
+
+app.use((err,req,res,next)=>{
+    console.error(err.stack)
+    res.status(500).send('someting broke    ')
+})
+
+app.listen(8000,(err)=>{
+    if(err){
+        console.log("server did not run")
+        return false
+    }
+
+    console.log("server is runnuing on the port : " + port )
+})
